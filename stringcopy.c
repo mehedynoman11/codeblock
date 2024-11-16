b666a84ba5ecddb518573a0c677ff2b7d33cab59
mehedynoman11 <mehedynoman11@gmail.com>
2024-11-16 11:26:35 +0600

    hhdf

--- stringcopy.c
+++ stringcopy.c
@@ -5,7 +5,7 @@ int main() {
     // Write Your Code Here
     char a[101], b[101];
     scanf("%s %s",a,b);
-    printf("%s %s",a,b);
+    // printf("%s %s",a,b);
     for (int i = 0; i <= strlen(b); i++)
     {
         a[i] = b[i];
